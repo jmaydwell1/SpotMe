a51720871f36afb2333e62ba51d6799398733ecc
nkumeh <umeh.k@northeastern.edu>
2021-11-14 04:16:45 -0500

    matched members recycler view

---
+++ app/src/main/java/edu/neu/madcourse/spotme/ListOfTheMatched.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.spotme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListOfTheMatched extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_the_matched);
+    }
+}
