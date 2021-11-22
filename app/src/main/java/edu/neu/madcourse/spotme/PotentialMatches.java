296a107ed67e27350b22d288e3079cf5ef90ac45
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-11-22 12:22:50 -0800

    connect potential matches page to database

--- app/src/main/java/edu/neu/madcourse/spotme/PotentialMatches.java
+++ app/src/main/java/edu/neu/madcourse/spotme/PotentialMatches.java
@@ -6,11 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -22,7 +18,8 @@ public class PotentialMatches extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private recyclerViewAdapter_sw adapter;
-    private Query db;
+    private FirebaseFirestore firebaseFirestore;
+    private Query query;
 
     ArrayList<userModel> userModelsList = new ArrayList<>();
 
@@ -32,31 +29,46 @@ public class PotentialMatches extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.potential_matches);
-
         recyclerView = findViewById(R.id.swRecyclerView);
 
-//        setUpUserModels();
-
-        // Pull from FirebaseDatabase
-        db = FirebaseFirestore.getInstance().collection("users");
-
-//        adapter = new recyclerViewAdapter_sw(this, userModelsList);
-//        recyclerView.setAdapter(adapter);
+        // Pull data from Firestore
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        query = firebaseFirestore.collection("users");
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
         populatePotentialMatches();
     }
 
-    private void setUpUserModels(){
-        String[] usersFullName = getResources().getStringArray(R.array.user_fullNames_all);
+//    private void setUpUserModels(){
+//        String[] usersFullName = getResources().getStringArray(R.array.user_fullNames_all);
+//
+//        for (int i = 0; i<usersFullName.length; i++){
+//            userModelsList.add(new userModel(usersFullName[i], userProfileImgs[i]));
+//        }
+//    }
 
-        for (int i = 0; i<usersFullName.length; i++){
-            userModelsList.add(new userModel(usersFullName[i], userProfileImgs[i]));
-        }
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override protected void onStart()
+    {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override protected void onStop()
+    {
+        super.onStop();
+        adapter.stopListening();
     }
 
     private void populatePotentialMatches() {
-        FirestoreRecyclerOptions<PotentialMatch> options = new FirestoreRecyclerOptions.Builder<PotentialMatch>().setQuery(db, PotentialMatch.class).build();
+        FirestoreRecyclerOptions<PotentialMatch> options = new FirestoreRecyclerOptions.Builder<PotentialMatch>()
+                .setQuery(query, PotentialMatch.class)
+                .build();
         adapter = new recyclerViewAdapter_sw(options);
         recyclerView.setAdapter(adapter);
     }
+
 }
--- app/src/main/java/edu/neu/madcourse/spotme/recyclerViewAdapter_sw.java
+++ app/src/main/java/edu/neu/madcourse/spotme/recyclerViewAdapter_sw.java
@@ -1,15 +1,12 @@
 package edu.neu.madcourse.spotme;
 
 import android.app.Dialog;
-import android.content.ContentValues;
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,13 +16,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
-import java.util.ArrayList;
-
 import edu.neu.madcourse.spotme.database.models.PotentialMatch;
 
 public class recyclerViewAdapter_sw extends FirestoreRecyclerAdapter<PotentialMatch, recyclerViewAdapter_sw.MyViewHolder> {
-    Context context;
-    ArrayList<userModel> userModels;
+//    Context context;
+//    ArrayList<userModel> userModels;
     Dialog dialogTest;
 
     public recyclerViewAdapter_sw(@NonNull FirestoreRecyclerOptions<PotentialMatch> options){
@@ -34,21 +29,20 @@ public class recyclerViewAdapter_sw extends FirestoreRecyclerAdapter<PotentialMa
 //        this.userModels = userModels;
     }
 
-    @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.recyclerview_row_sw, parent, false);
-//        final MyViewHolder viewHolder = new MyViewHolder(view);
+        final MyViewHolder viewHolder = new MyViewHolder(view);
 
-//        dialogTest = new Dialog(context);
-//        dialogTest.setContentView(R.layout.potential_buddy_dialog);
-//        dialogTest.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialogTest = new Dialog(parent.getContext());
+        dialogTest.setContentView(R.layout.potential_buddy_dialog);
+        dialogTest.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-//        viewHolder.potentialMatchCard.setOnClickListener(itemView -> dialogTest.show());
+        viewHolder.potentialMatchCard.setOnClickListener(itemView -> dialogTest.show());
 
-        return new MyViewHolder(view);
+        return viewHolder;
     }
 
 //    @Override
@@ -65,21 +59,16 @@ public class recyclerViewAdapter_sw extends FirestoreRecyclerAdapter<PotentialMa
         holder.textView.setText(name);
     }
 
-//    @Override
-//    public int getItemCount() {
-//        return userModels.size();
-//    }
+    public class MyViewHolder extends RecyclerView.ViewHolder{
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
-
-        ImageView imageView;
+//        ImageView imageView;
         TextView textView;
         CardView potentialMatchCard;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            imageView = itemView.findViewById(R.id.userImg);
+//            imageView = itemView.findViewById(R.id.userImg);
             textView = itemView.findViewById(R.id.fullName);
             potentialMatchCard = itemView.findViewById(R.id.potential_match_card);
 
