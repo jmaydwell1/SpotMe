e3c156ec0509be095b8dac7166fa20ce4b168371
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-11-25 13:57:30 -0800

    Connect main match activity to database

--- app/src/main/java/edu/neu/madcourse/spotme/MainMatchMessageActivity.java
+++ app/src/main/java/edu/neu/madcourse/spotme/MainMatchMessageActivity.java
@@ -7,8 +7,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.neu.madcourse.spotme.database.models.Match;
 
 public class MainMatchMessageActivity extends AppCompatActivity {
 
@@ -16,16 +22,16 @@ public class MainMatchMessageActivity extends AppCompatActivity {
     LinearLayoutManager linearLayoutManager;
     List<MessageMatchModel> matchList;
     MatchesRViewAdapter adapter;
-
+    FirebaseFirestore firebaseFirestore;
+    Query query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_matches);
 
-       initialDate();
+//       initialDate();
        initialRView();
-
     }
 
     private void initialRView() {
@@ -34,9 +40,30 @@ public class MainMatchMessageActivity extends AppCompatActivity {
         linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        adapter = new MatchesRViewAdapter(matchList);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        query = firebaseFirestore.collection("matches");
+        FirestoreRecyclerOptions<Match> options = new FirestoreRecyclerOptions.Builder<Match>()
+                .setQuery(query, Match.class)
+                .build();
+        adapter = new MatchesRViewAdapter(options);
         recyclerView.setAdapter(adapter);
        // adapter.notifyDataSetChanged();
+    }
+
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     private void initialDate() {
