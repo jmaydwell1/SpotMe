71871e778433bc212f8cc2ed5c144bf04228d3ee
jmaydwell1 <77993695+jmaydwell1@users.noreply.github.com>
2021-12-06 00:11:19 -0500

    Add files via upload

--- app/src/main/java/edu/neu/madcourse/spotme/ProfileBuilder.java
+++ app/src/main/java/edu/neu/madcourse/spotme/ProfileBuilder.java
@@ -1,42 +1,110 @@
 package edu.neu.madcourse.spotme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.GridLayout;
-import android.widget.Toast;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+public class ProfileBuilder extends AppCompatActivity {
 
-public class ProfileBuilder extends AppCompatActivity  {
-
-    GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_builder);
 
-        gridLayout=(GridLayout) gridLayout.findViewById(R.id.mainGrid);
+        Button buttonOne = findViewById(R.id.imageView5);
+        Button buttonTwo = findViewById(R.id.imageView6);
+        Button buttonThree = findViewById(R.id.imageView7);
+        Button buttonFour = findViewById(R.id.imageView8);
+        Button buttonFive = findViewById(R.id.imageView9);
+        Button buttonSix = findViewById(R.id.imageView10);
 
-        setSingleEvent(gridLayout);
 
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPreferenceActivity();
+            }
+
+            public void openPreferenceActivity() {
+                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
+                String strName = null;
+                intent.putExtra("ping pong", strName);
+                startActivity(intent);
+            }
+        });
+
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPreferenceActivity();
+            }
+
+            public void openPreferenceActivity() {
+                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
+                String strName = null;
+                intent.putExtra("running", strName);
+                startActivity(intent);
+            }
+        });
+
+        buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPreferenceActivity();
+            }
+
+            public void openPreferenceActivity() {
+                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
+                String strName = null;
+                intent.putExtra("ski", strName);
+                startActivity(intent);
+            }
+        });
+
+        buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPreferenceActivity();
+            }
+
+            public void openPreferenceActivity() {
+                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
+                String strName = null;
+                intent.putExtra("soccer", strName);
+                startActivity(intent);
+            }
+        });
+
+        buttonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPreferenceActivity();
+            }
+
+            public void openPreferenceActivity() {
+                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
+                String strName = null;
+                intent.putExtra("swimming", strName);
+                startActivity(intent);
+            }
+        });
+
+        buttonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPreferenceActivity();
+            }
+
+            public void openPreferenceActivity() {
+                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
+                String strName = null;
+                intent.putExtra("swimming", strName);
+                startActivity(intent);
+            }
+        });
     }
-
-    // we are setting onClickListener for each element
-    private <CardView> void setSingleEvent(GridLayout gridLayout) {
-        for(int i = 0; i<gridLayout.getChildCount();i++){
-            CardView cardView=(CardView)gridLayout.getChildAt(i);
-            final int finalI= i;
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(ProfileBuilder.this,"Clicked at index "+ finalI,
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
-
-
 }
