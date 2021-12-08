70bd995c7048438d913f733a707898c2eee8483b
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-12-07 18:10:47 -0800

    Onclick for sport icons

--- app/src/main/java/edu/neu/madcourse/spotme/ProfileBuilder.java
+++ app/src/main/java/edu/neu/madcourse/spotme/ProfileBuilder.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.spotme;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -13,109 +15,26 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ProfileBuilder extends AppCompatActivity {
 
     private ImageView nextBtn;
+    private ImageButton pingPongBtn, runningBtn, skiBtn, soccerBtn, swimmingBtn, yogaBtn;
+    private boolean pingPongBtnPressed, runningBtnPressed, yogaBtnPressed, skiBtnPressed, soccerBtnPressed, swimmingBtnPressed;
 
+    private static final String TAG = "ProfileBuilder";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_builder);
 
-        ImageButton pingPongBtn = findViewById(R.id.potentialBuilderPingPong);
-        ImageButton runningBtn = findViewById(R.id.potentialBuilderRunning);
-        ImageButton skiBtn = findViewById(R.id.potentialBuilderSki);
-        ImageButton soccerBtn = findViewById(R.id.potentialBuilderSoccer);
-        ImageButton swimmingBtn = findViewById(R.id.potentialBuilderSwimming);
-        ImageButton yogaBtn = findViewById(R.id.potentialBuilderYoga);
-
+        pingPongBtn = findViewById(R.id.potentialBuilderPingPong);
+        runningBtn = findViewById(R.id.potentialBuilderRunning);
+        skiBtn = findViewById(R.id.potentialBuilderSki);
+        soccerBtn = findViewById(R.id.potentialBuilderSoccer);
+        swimmingBtn = findViewById(R.id.potentialBuilderSwimming);
+        yogaBtn = findViewById(R.id.potentialBuilderYoga);
         nextBtn = findViewById(R.id.potentialBuilderNext);
 
-        Drawable d = getResources().getDrawable(R.drawable.round_background_dark);
-
-        pingPongBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pingPongBtn.setBackgroundResource(R.color.cardview_dark_background);
-                openPreferenceActivity();
-            }
-
-            public void openPreferenceActivity() {
-                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
-                String strName = "ping pong";
-                intent.putExtra("ping pong", strName);
-            }
-        });
-
-        runningBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                runningBtn.setBackgroundResource((R.color.cardview_dark_background));
-                openPreferenceActivity();
-            }
-
-            public void openPreferenceActivity() {
-                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
-                String strName = "running";
-                intent.putExtra("running", strName);
-            }
-        });
-
-        skiBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                skiBtn.setBackgroundResource((R.color.cardview_dark_background));
-                openPreferenceActivity();
-            }
-
-            public void openPreferenceActivity() {
-                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
-                String strName = "ski";
-                intent.putExtra("ski", strName);
-            }
-        });
-
-        soccerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                soccerBtn.setBackgroundResource((R.color.cardview_dark_background));
-                openPreferenceActivity();
-            }
-
-            public void openPreferenceActivity() {
-                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
-                String strName = "soccer";
-                intent.putExtra("soccer", strName);
-            }
-        });
-
-        swimmingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                swimmingBtn.setBackgroundResource((R.color.cardview_dark_background));
-                openPreferenceActivity();
-            }
-
-            public void openPreferenceActivity() {
-                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
-                String strName = "swimming";
-                intent.putExtra("swimming", strName);
-            }
-        });
-
-        yogaBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                yogaBtn.setBackground(R.drawable.round_background_dark);
-                yogaBtn.setBackground(d);
-//                yogaBtn.setBackgroundResource(R.drawable.round_background_dark);
-                openPreferenceActivity();
-            }
-
-            public void openPreferenceActivity() {
-                Intent intent = new Intent(ProfileBuilder.this, Preference.class);
-                String strName = null;
-                intent.putExtra("swimming", strName);
-            }
-        });
+        ButtonClickListener buttonClickListener = new ButtonClickListener();
+        pingPongBtn.setOnClickListener(buttonClickListener);
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,5 +43,49 @@ public class ProfileBuilder extends AppCompatActivity {
                 ProfileBuilder.this.startActivity(preferenceIntent);
             }
         });
+    }
+
+    class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            sportsButtonOnClick(view);
+        }
+    }
+
+    public void sportsButtonOnClick(View v) {
+        switch(v.getId()) {
+            case R.id.potentialBuilderPingPong:
+                Log.d(TAG, "Ping Pong is pressed");
+                switchButtonState(pingPongBtnPressed, pingPongBtn);
+                pingPongBtnPressed = !pingPongBtnPressed;
+                break;
+            case R.id.potentialBuilderRunning:
+                switchButtonState(runningBtnPressed, runningBtn);
+                runningBtnPressed = !runningBtnPressed;
+                break;
+            case R.id.potentialBuilderSki:
+                switchButtonState(skiBtnPressed, skiBtn);
+                skiBtnPressed = ! skiBtnPressed;
+                break;
+            case R.id.potentialBuilderSoccer:
+                switchButtonState(soccerBtnPressed, soccerBtn);
+                soccerBtnPressed = !soccerBtnPressed;
+                break;
+            case R.id.potentialBuilderSwimming:
+                switchButtonState(swimmingBtnPressed, swimmingBtn);
+                swimmingBtnPressed = !swimmingBtnPressed;
+                break;
+            case R.id.potentialBuilderYoga:
+                switchButtonState(yogaBtnPressed, yogaBtn);
+                yogaBtnPressed = !yogaBtnPressed;
+        }
+    }
+
+    private void switchButtonState(boolean imageButtonState, ImageButton imageButton) {
+        if (imageButtonState) {
+            imageButton.setBackgroundResource(R.drawable.round_background_dark);
+        } else {
+            imageButton.setBackgroundResource(R.drawable.round_background_light);
+        }
     }
 }
