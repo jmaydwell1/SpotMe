cbc388ca3d4bfab87a5793bbb06021980123cfd4
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-12-08 16:02:19 -0800

    Add back progress bar

--- app/src/main/java/edu/neu/madcourse/spotme/SpotMeSplashScreen.java
+++ app/src/main/java/edu/neu/madcourse/spotme/SpotMeSplashScreen.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class SpotMeSplashScreen extends AppCompatActivity {
     private static final Integer DELAY_TIME = 1000;
